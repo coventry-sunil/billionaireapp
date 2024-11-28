e72d6a2a7d2ec63b2e520ebf19778fb404caef4a
Sunil Shrestha <shresth90@uni.coventry.ac.uk>
2024-11-28 21:25:59 +0545

    working with buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,34 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void buttonFunction() {
+    print("Button is pressed!!");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
       home: Scaffold(
-          appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Billionaire App"),
-      )),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Billionaire App"),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          color: Colors.blueGrey[700],
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Balance Part"),
+              ElevatedButton(
+                  onPressed: buttonFunction, child: Text("Click Here"))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
