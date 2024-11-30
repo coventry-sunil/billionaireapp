7ba408911a660011d58f781c4ff5e2d8b7b86851
Sunil Shrestha <shresth90@uni.coventry.ac.uk>
2024-11-30 20:29:22 +0545

    expanded, styling and button styling

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,53 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Text("Balance Part"),
-              ElevatedButton(
-                  onPressed: buttonFunction, child: const Text("Click Here"))
+              const Expanded(
+                flex: 9,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Bank Balance: "),
+                    SizedBox(height: 20),
+                    Text("0")
+                  ],
+                ),
+              ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [
+              //     Expanded(
+              //       child: Container(
+              //         color: Colors.red,
+              //         child: Text("One"),
+              //       ),
+              //     ),
+              //     Expanded(
+              //       child: Container(
+              //         color: Colors.green,
+              //         child: Text("Two"),
+              //       ),
+              //     ),
+              //     Expanded(
+              //       child: Container(
+              //         color: Colors.blue,
+              //         child: Text("Three"),
+              //       ),
+              //     )
+              //   ],
+              // ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red[700],
+                            minimumSize: const Size(double.infinity, 55)),
+                        onPressed: buttonFunction,
+                        child: const Text("Add Money")),
+                  ],
+                ),
+              )
             ],
           ),
         ),
